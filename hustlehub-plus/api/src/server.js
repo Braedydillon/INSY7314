7ec2888c67bf19c63473e76e568463a7982c41d6
Braedydillon <ST10439231@vcconnect.edu.za>
2026-09-03 17:43:47 +0200

    - Added certificates  for secure https

--- hustlehub-plus/api/src/server.js
+++ hustlehub-plus/api/src/server.js
@@ -1,6 +1,14 @@
+import fs from "fs";
+import https from "https";
 import app from "./app.js";
 import config from "./config/env.js";
 
-app.listen(config.PORT, () => {
-  console.log(`Server is running on port ${config.PORT}`);
+
+const options = {
+  key: fs.readFileSync("./certs/key.pem"),
+  cert: fs.readFileSync("./certs/cert.pem")
+};
+
+https.createServer(options, app).listen(config.PORT, () => {
+  console.log(`Server is running securely on https://localhost:${config.PORT}`);
 });
